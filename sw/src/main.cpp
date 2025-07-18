4c46e14f561bd958e7154587f84234f8102c7c5c
Bruce MacKinnon <bruce@mackinnon.com>
2025-07-18 14:22:59 -0400

    Got a version of arm_fir_interpolate_f32 working

--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -304,10 +304,10 @@ static void process_in_frame() {
     else if (audioSource1.getSource() == AudioSourceControl::RADIO1)
         r1_cross_gains[1] = 1.0;
 
-    core0.cycle0(r0_samples, r0_cross);
-    core1.cycle0(r1_samples, r1_cross);
-    core0.cycle1(2, cross_ins, r0_cross_gains, r0_out);
-    core1.cycle1(2, cross_ins, r1_cross_gains, r1_out);
+    core0.cycleRx(r0_samples, r0_cross);
+    core1.cycleRx(r1_samples, r1_cross);
+    core0.cycleTx(2, cross_ins, r0_cross_gains, r0_out);
+    core1.cycleTx(2, cross_ins, r1_cross_gains, r1_out);
 
     // Re-pack data for DAC
     j = 0;
