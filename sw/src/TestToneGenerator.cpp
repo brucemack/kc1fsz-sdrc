259c9dfc5754278927db7cc1023392b23cf5851d
Bruce MacKinnon <bruce@mackinnon.com>
2025-07-20 13:44:39 -0400

    Added new state for testing

---
+++ sw/src/TestToneGenerator.cpp
@@ -0,0 +1,59 @@
+/**
+ * Software Defined Repeater Controller
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+ */
+#include "TestToneGenerator.h"
+#include "AudioCore.h"
+
+#define TEST_TONE_DUR_MS (30 * 1000)
+
+namespace kc1fsz {
+
+TestToneGenerator::TestToneGenerator(Log& log, Clock& clock, AudioCore& core) 
+:   _log(log),
+    _clock(clock),
+    _core(core)
+{
+}
+
+void TestToneGenerator::run() {
+    if (_running) {
+        if (_clock.isPast(_endTime)) {
+            _running = false;
+            _core.setToneEnabled(false);
+            _log.info("Test tone end");
+        }
+    }
+}
+
+void TestToneGenerator::start() {
+    _running = true;
+    // This is set in the past so that we immediately start working on the first
+    // symbol of the ID.
+    _endTime = _clock.time() + TEST_TONE_DUR_MS;
+    _core.setToneFreq(_freq);
+    _core.setToneLevel(_level);
+    _core.setToneEnabled(true);
+    _log.info("Test tone start");
+}
+
+bool TestToneGenerator::isFinished() {
+    return !_running;
+}
+
+}
---
+++ sw/src/TestToneGenerator.h
@@ -0,0 +1,58 @@
+/**
+ * Software Defined Repeater Controller
+ * Copyright (C) 2025, Bruce MacKinnon KC1FSZ
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOT FOR COMMERCIAL USE WITHOUT PERMISSION.
+ */
+#ifndef _TestToneGenerator_h
+#define _TestToneGenerator_h
+
+#include "kc1fsz-tools/Log.h"
+#include "kc1fsz-tools/Clock.h"
+
+#include "ToneGenerator.h"
+
+namespace kc1fsz {
+
+class AudioCore;
+
+class TestToneGenerator : public ToneGenerator {
+public:
+
+    TestToneGenerator(Log& log, Clock& clock, AudioCore& core);
+
+    virtual void run();
+
+    virtual void start();
+    virtual bool isFinished();
+    void setFreq(float hz) { _freq = hz; }
+    void setLevel(float db) { _level = db; }
+
+private:
+
+    Log& _log;
+    Clock& _clock;
+    AudioCore& _core;
+
+    float _freq = 1000.0;
+    float _level = -10.0;    
+    bool _running = false;
+    uint32_t _endTime = 0;
+};
+
+}
+
+#endif
