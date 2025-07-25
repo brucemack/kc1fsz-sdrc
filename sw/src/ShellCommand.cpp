d6a403d97724d53675e3e76308e38cbace8de464
brucemack <bruce@mackinnon.com>
2025-07-25 13:26:48 -0400

    Added configuration parameter for DTMF detection level

--- sw/src/ShellCommand.cpp
+++ sw/src/ShellCommand.cpp
@@ -212,6 +212,13 @@ void ShellCommand::process(const char* cmd) {
                     _config.rx1.agcLevel = atof(tokens[3]);
                 else 
                     printf(INVALID_COMMAND);                
+            else if (eq(tokens[1], "dtmflevel"))
+                if (eq(tokens[2], "0"))
+                    _config.rx0.dtmfDetectLevel = atof(tokens[3]);
+                else if (eq(tokens[2], "1"))
+                    _config.rx1.dtmfDetectLevel = atof(tokens[3]);
+                else 
+                    printf(INVALID_COMMAND);                
             else if (eq(tokens[1], "txenable"))
                 if (eq(tokens[2], "0"))
                     _config.tx0.enabled = atoi(tokens[3]) == 1;
